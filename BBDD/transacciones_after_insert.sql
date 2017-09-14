2460fa3c128c70f6087ed4ff0abd8598c6a528e0
Pablo Godoy <pablodavid36@gmail.com>
2017-09-13 22:31:42 -0600

    BBDD (trigger): transacciones insert/update

--- BBDD/transacciones_after_insert.sql
+++ BBDD/transacciones_after_insert.sql
@@ -1,8 +1,14 @@
 DELIMITER $$
+
+DROP TRIGGER IF EXISTS transacciones_after_insert$$
+
 CREATE TRIGGER transacciones_after_insert AFTER INSERT ON transacciones FOR EACH ROW begin
-  if NEW.estado = 'Solicitado' then
-    update cuentas set reserva = reserva + NEW.monto where id = NEW.cuenta_id;
-  end if;
+    if NEW.estado = 'Solicitado' then
+        update cuentas set reserva = reserva + NEW.monto where id = NEW.cuenta_id;
+    elseif NEW.estado = 'Autorizado' then
+        update cuentas set balance = balance + NEW.monto where id = NEW.cuenta_id;
+    end if;
 end
+
 $$
 DELIMITER ;
---
+++ BBDD/transacciones_after_update.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS transacciones_after_update$$
+
+CREATE TRIGGER transacciones_after_update AFTER UPDATE ON transacciones FOR EACH ROW begin
+    if OLD.estado = 'Solicitado' AND NEW.estado = 'Autorizado' then
+        update cuentas set reserva = reserva - OLD.monto, balance = balance + OLD.monto where id = NEW.cuenta_id;
+    elseif OLD.estado = 'Solicitado' AND NEW.estado = 'Rechazado' then
+        update cuentas set balance = balance - OLD.monto where id = NEW.cuenta_id;
+    end if;
+end
+
+$$
+DELIMITER ;
