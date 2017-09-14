b9206049b936c639727f5e43a38e1a504c044e0c
Pablo Godoy <pablodavid36@gmail.com>
2017-09-13 22:39:15 -0600

    App (Config): conexion a base de datos

--- config/app.php
+++ config/app.php
@@ -229,9 +229,9 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => 'usr_dwbanco',
+            'password' => 'oDg3axSwtWOd3IHm',
+            'database' => 'dw-banco',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
