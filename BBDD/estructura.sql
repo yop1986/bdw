389dbaa2cbb11a089006a334ae8f3b91513c558c
Pablo Godoy <pablodavid36@gmail.com>
2017-09-14 00:04:00 -0600

    BBDD (estructura): longitud columna cuentas

--- BBDD/estructura.sql
+++ BBDD/estructura.sql
@@ -3,14 +3,14 @@
 
 CREATE TABLE usuarios (
     id smallint UNSIGNED NOT NULL AUTO_INCREMENT,
-    nombre varchar(60) COLLATE utf8_spanish_ci NOT NULL,
-    correo varchar(120) COLLATE utf8_spanish_ci NOT NULL,
-    telefono varchar(11) COLLATE utf8_spanish_ci NOT NULL,
-    contrasena varchar(150) COLLATE utf8_spanish_ci NOT NULL,
+    nombre varchar(60) NOT NULL,
+    correo varchar(120) NOT NULL,
+    telefono varchar(11) NOT NULL,
+    contrasena varchar(150) NOT NULL,
     creado datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modificado datetime DEFAULT NULL,
     activo tinyint(1) NOT NULL DEFAULT 0,
-    grupo enum('Administrador','Cliente') COLLATE utf8_spanish_ci NOT NULL DEFAULT 'Cliente',
+    grupo enum('Administrador','Cliente') NOT NULL DEFAULT 'Cliente',
 
     CONSTRAINT usuarios_pk_id PRIMARY KEY (id),
     CONSTRAINT usuarios_unq_correo UNIQUE (correo)
@@ -18,8 +18,8 @@ CREATE TABLE usuarios (
 
 CREATE TABLE cuentas (
     id mediumint UNSIGNED NOT NULL AUTO_INCREMENT,
-    nombre varchar(30) COLLATE utf8_spanish_ci NOT NULL,
-    cuenta varchar(8) COLLATE utf8_spanish_ci NOT NULL,
+    nombre varchar(30) NOT NULL,
+    cuenta varchar(14) NOT NULL,
     balance decimal(15,2) NOT NULL,
     reserva decimal(15,2) NOT NULL DEFAULT '0.00',
     creado datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -29,6 +29,7 @@ CREATE TABLE cuentas (
 );
 #ALTER TABLE cuentas DROP FOREIGN KEY cuentas_fk_usuario
 #ALTER TABLE cuentas DROP COLUMN usuario_id
+#ALTER TABLE cuentas MODIFY COLUMN cuenta varchar(14) NOT NULL
 
 CREATE TABLE transacciones (
     id bigint UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -53,7 +54,7 @@ CREATE TABLE transacciones (
 --      id int UNSIGNED NOT NULL,
 --      monto_max decimal(15,2) UNSIGNED NOT NULL,
 --      cant_max tinyint(3) UNSIGNED NOT NULL,
---      clave varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+--      clave varchar(30) NOT NULL,
 --      estado tinyint(1) NOT NULL DEFAULT '0',
 --      usuario_id smallint UNSIGNED NOT NULL,
 --      cuenta_id mediumint UNSIGNED NOT NULL
