35d119b719b4bd2d54267cdef4aae4c31a685bd5
Pablo Godoy <pablodavid36@gmail.com>
2017-09-16 08:23:04 -0600

    App (AppController): comentario para agregar metodos sin autenticacion

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -30,7 +30,7 @@ class AppController extends Controller
 
     public function beforeFilter(Event $event)
     {
-        $this->Auth->allow(['*']);
+        //$this->Auth->allow();
     }
 
     /**
